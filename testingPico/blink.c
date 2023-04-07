52f70f10afd5fc74e92a3ec077dc8a83e7bdd97b
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-04-07 17:39:17 -0400

    Working on writing code to the pico in C

---
+++ testingPico/blink.c
@@ -0,0 +1,30 @@
+// Taken from digi-key pico starter code 
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define true 1
+#define false 0
+
+int main() {
+
+    const int led_pin = 25;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Loop forever
+    while (true) {
+
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
+}
