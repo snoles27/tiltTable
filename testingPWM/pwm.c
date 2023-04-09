d4f73c744f1eabeb6313ab2f10cac954c6fe0769
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-04-08 20:00:07 -0400

    added serial read to control servo position

--- testingPWM/pwm.c
+++ testingPWM/pwm.c
@@ -5,7 +5,7 @@
 
 const uint prim_pin = 15;
 const uint16_t wrap = 12500;
-const uint16_t level = 6250;
+const uint16_t initLevel = 6250;
 const uint8_t clkdiv_int = 40;
 const uint8_t clkdiv_frac = 0;
 
@@ -20,14 +20,24 @@ int main() {
 
     pwm_set_clkdiv_int_frac(slice, clkdiv_int, clkdiv_frac);
     pwm_set_wrap(slice, wrap);
-    pwm_set_chan_level(slice, channel, level);
+    pwm_set_chan_level(slice, channel, initLevel);
     pwm_set_phase_correct(slice, 0);
 
     pwm_set_enabled(slice, 1);
 
+    stdio_init_all();
 
-     while (1)
-        tight_loop_contents();
+    int level = initLevel;
+
+     while (1) {
+
+        printf("Enter a level between 3125 and 6250\n");
+        scanf("%d", &level);
+        pwm_set_chan_level(slice, channel, level);
+
+        sleep_ms(500);
+
+     }
     
 
 }
