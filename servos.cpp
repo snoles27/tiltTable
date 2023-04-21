f1c34d74ecbc583059bf3ec6a493eddc6bb06ae5
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-04-20 20:14:34 -0400

    commented servos.cpp\

--- servos.cpp
+++ servos.cpp
@@ -4,7 +4,7 @@
 #include "servos.h"
 #include <string>
 
-//setting numbers that are useful for talking about anlges in radians
+//defining useful numbers
 #define PI 3.1415926
 #define PIHALF 1.570796
 
@@ -24,6 +24,12 @@ const double servo_angleMax[2] = {PIHALF, PIHALF};
 const int servo_levelMax[2] = {5576,13895};
 
 void setPosition(int servoNum, double angle, bool manual){
+    //moves servo to set angle using parameters hardcoded at top of file
+
+    //inputs//
+    //servoNum: 0 or 1 sepecifying which servo to move
+    //angle: angle in radians to move the servo
+    //maunal: boolean variable which has function do more checks before it moves it
 
     // make sure the set angle is within the allowed range. If not set to max or min. 
     if(manual){
@@ -33,11 +39,12 @@ void setPosition(int servoNum, double angle, bool manual){
             angle = servo_angleMin[servoNum];
         } else {}
     }
+
     //get slice and channel for servoPin
     unsigned int slice=pwm_gpio_to_slice_num(servoPin[servoNum]);
     unsigned int channel=pwm_gpio_to_channel(servoPin[servoNum]);
 
-    //get level2angle conversion
+    //convert the angle to pwm level
     double level2angle;
     level2angle = (double)(servo_levelMax[servoNum] - servo_levelMin[servoNum])/(servo_angleMax[servoNum]-servo_angleMin[servoNum]);
     int level;
@@ -50,11 +57,16 @@ void setPosition(int servoNum, double angle, bool manual){
         printf("\n");
     }
 
+    //set the channel level
     pwm_set_chan_level(slice, channel, level);
 
 }
 
 void initServo(int servoNum) {
+    // initializes pwm for servo motion using hardcoded parameters at top of page
+
+    //inputs//
+    //servoNum: servo index to set up
 
     gpio_set_function(servoPin[servoNum], GPIO_FUNC_PWM);
     unsigned int slice=pwm_gpio_to_slice_num(servoPin[servoNum]);
@@ -70,23 +82,32 @@ void initServo(int servoNum) {
 
 }
 
-void sweepServos(float initServo1, float initServo2, float finalServo1, float finalServo2, double seconds){
+void sweepServos(float initServo0, float initServo1, float finalServo0, float finalServo1, double seconds){
+    //sweeps servos simultaneously from inital angles to final angle in set time
+
+    //inputs//
+    //initServo0: inital angle of servo 0 (radians)
+    //initServo1: intial angle of servo 1 (radians)
+    //finalServo0: final angle of servo 0 (radians)
+    //finalServo1: final angle of servo 1 (radians)
+    //seconds: time to perform move (seconds)
 
     int numSteps = 100; //number of steps to break the sweep into
-    int timeStep;
-    timeStep = (seconds/numSteps) * 1000000.0; //time step in us
-    float servo1Step;
-    servo1Step = (finalServo1 - initServo1)/numSteps;
-    float servo2Step;
-    servo2Step = (finalServo2 - initServo2)/numSteps;
 
-    //move servos to inital position
-    setPosition(0, initServo1, false);
-    setPosition(1, initServo2, false);
+    int timeStep; //time step in us
+    timeStep = (seconds/numSteps) * 1000000.0; 
+    float servo0Step; //amount to change servo angle 0 angle per time step 
+    servo0Step = (finalServo0 - initServo0)/numSteps; 
+    float servo1Step; //amount to change servo angle 1 angle per time step 
+    servo1Step = (finalServo1 - initServo1)/numSteps;
+
+    //move servos to inital position (often will already be there)
+    setPosition(0, initServo0, false);
+    setPosition(1, initServo1, false);
 
     for(int i = 1; i <= numSteps; i++){
-        setPosition(0, initServo1 + i * servo1Step, false);
-        setPosition(1, initServo2 + i * servo2Step, false);
+        setPosition(0, initServo0 + i * servo0Step, false);
+        setPosition(1, initServo1 + i * servo1Step, false);
         sleep_us(timeStep);
     }
 
