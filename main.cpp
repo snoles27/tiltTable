93d3bcbfc0dccff31f56a618877ad2d5220b6b25
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-04-24 14:14:28 -0400

    quick move in circle demo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include "hardware/pwm.h"
 #include "servos.h"
 #include <string>
+#include <cmath>
 
+#define PI 3.1415926
 
 int main() {
  
@@ -15,6 +17,11 @@ int main() {
     float angleRead = 0.0;
     int servoNumber = 0;
 
+    float amplitude = 0.8;
+    float freq = 0.5;
+    float timeStep = 0.02; //servo motor timestep (s)
+    int numRotations = 5;
+    int numStep = (int)((1/timeStep)/freq * numRotations);
 
     float pos1 = 0.0;
     float pos2 = 0.0;
@@ -24,14 +31,30 @@ int main() {
 
     while (1) {
 
-        scanf("%f", &pos1);
-        scanf("%f", &pos2);
-        scanf("%f", &time);
+        // //moving in circle
+        setPosition(0, amplitude, false);
+        setPosition(1, 0., false);
+        sleep_ms(6000);
+        std::string dis = "hold";
+        for(int i = 0; i < numStep; i++){
+            pos1 = amplitude * std::cos(2. * PI * i * timeStep * freq);
+            pos2 = amplitude * std::sin(2. * PI * i * timeStep * freq);
+            setPosition(0, pos1, false);
+            setPosition(1, pos2, false);
+            // dis = std::to_string(pos1);
+            // printf(dis.c_str());
+            // printf("\n");
+            sleep_ms((int)(timeStep * 1000));
+        }
 
-        sweepServos(initPos1, initPos2, pos1, pos2, time);
-        initPos1 = pos1;
-        initPos2 = pos2;
-        printf("Complete! \n");
+        // scanf("%f", &pos1);
+        // scanf("%f", &pos2);
+        // scanf("%f", &time);
+
+        // sweepServos(initPos1, initPos2, pos1, pos2, time);
+        // initPos1 = pos1;
+        // initPos2 = pos2;
+        // printf("Complete! \n");
 
         // printf("Enter a servo number: \n");
         // scanf("%d", &servoNumber);
