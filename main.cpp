62a9d2a8bb5cfecdaad73bdafff7f3d1812c8f18
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-04-26 18:28:45 -0400

    about to cut the eigen library

--- main.cpp
+++ main.cpp
@@ -8,44 +8,50 @@
 #define PI 3.1415926
 
 int main() {
- 
-    initServo(0);
-    initServo(1);
+    
+    // testing solvers
 
-    stdio_init_all();
 
-    float angleRead = 0.0;
-    int servoNumber = 0;
 
-    float amplitude = 0.8;
-    float freq = 0.5;
-    float timeStep = 0.02; //servo motor timestep (s)
-    int numRotations = 5;
-    int numStep = (int)((1/timeStep)/freq * numRotations);
+    // testing with stand
 
-    float pos1 = 0.0;
-    float pos2 = 0.0;
-    float initPos1 = 0.0;
-    float initPos2 = 0.0;
-    float time = 0.0;
+    // initServo(0);
+    // initServo(1);
 
-    while (1) {
+    // stdio_init_all();
 
-        // //moving in circle
-        setPosition(0, amplitude, false);
-        setPosition(1, 0., false);
-        sleep_ms(6000);
-        std::string dis = "hold";
-        for(int i = 0; i < numStep; i++){
-            pos1 = amplitude * std::cos(2. * PI * i * timeStep * freq);
-            pos2 = amplitude * std::sin(2. * PI * i * timeStep * freq);
-            setPosition(0, pos1, false);
-            setPosition(1, pos2, false);
-            // dis = std::to_string(pos1);
-            // printf(dis.c_str());
-            // printf("\n");
-            sleep_ms((int)(timeStep * 1000));
-        }
+    // float angleRead = 0.0;
+    // int servoNumber = 0;
+
+    // float amplitude = 0.8;
+    // float freq = 0.5;
+    // float timeStep = 0.02; //servo motor timestep (s)
+    // int numRotations = 5;
+    // int numStep = (int)((1/timeStep)/freq * numRotations);
+
+    // float pos1 = 0.0;
+    // float pos2 = 0.0;
+    // float initPos1 = 0.0;
+    // float initPos2 = 0.0;
+    // float time = 0.0;
+
+    // while (1) {
+
+    //     // //moving in circle
+    //     setPosition(0, amplitude, false);
+    //     setPosition(1, 0., false);
+    //     sleep_ms(6000);
+    //     std::string dis = "hold";
+    //     for(int i = 0; i < numStep; i++){
+    //         pos1 = amplitude * std::cos(2. * PI * i * timeStep * freq);
+    //         pos2 = amplitude * std::sin(2. * PI * i * timeStep * freq);
+    //         setPosition(0, pos1, false);
+    //         setPosition(1, pos2, false);
+    //         // dis = std::to_string(pos1);
+    //         // printf(dis.c_str());
+    //         // printf("\n");
+    //         sleep_ms((int)(timeStep * 1000));
+    //     }
 
         // scanf("%f", &pos1);
         // scanf("%f", &pos2);
@@ -64,6 +70,6 @@ int main() {
         // setPosition(servoNumber, angleRead, true);
         // sleep_ms(500);
 
-     }
+    // }
 
 }
